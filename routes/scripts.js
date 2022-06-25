db383f18e69c8c5f9fcec54395644fb28e5fb711
Fabricio Videira <fabricio@allcomsistemas.com.br>
2022-06-25 14:12:54 -0300

    :wrench: Tratando erros

--- routes/scripts.js
+++ routes/scripts.js
@@ -1,13 +1,32 @@
 const express = require("express");
+const endpoint = require("../functions/endpoint");
+const HttpError = require("../utils/HttpError");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ message: "ola" });
-});
+router.get(
+  "/",
+  endpoint(async (req, res) => {
+    if (Math.random() > 0.5) {
+      throw new HttpError(400, "erro");
+    }
 
-router.get("/teste", (req, res) => {
-  res.json({ message: "ola na rota teste" });
-});
+    res.json({ message: "ola" });
+  })
+);
+
+router.post(
+  "/aaaa",
+  endpoint(async (req, res) => {
+    res.json({ message: "post" });
+  })
+);
+
+router.get(
+  "/teste",
+  endpoint(async (req, res) => {
+    res.json({ message: "ola 2" });
+  })
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,21 @@ app.use((req, res) => {
   res.status(404).json({ message: "resource not found" });
 });
 
+app.use((err, req, res, next) => {
+  let status = err.status || 500;
+  let message = err.message;
+
+  if (status === 500) {
+    console.error(err);
+
+    if (process.env.NODE_ENV === "production") {
+      message = "Internal server error";
+    }
+  }
+
+  res.status(status).json({ message });
+});
+
 app.listen(3000, () => {
   console.log("http://localhost:3000");
 });

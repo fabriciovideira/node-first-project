6149a95d2f63e0287b4d07aa9a25a195d522c577
Fabricio Videira <fabricio@allcomsistemas.com.br>
2022-06-25 13:55:12 -0300

    Change response to JSON

--- routes/scripts.js
+++ routes/scripts.js
@@ -3,11 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("hello");
+  res.json({ message: "ola" });
 });
 
 router.get("/teste", (req, res) => {
-  res.send("ol222a 123");
+  res.json({ message: "ola na rota teste" });
 });
 
 module.exports = router;
